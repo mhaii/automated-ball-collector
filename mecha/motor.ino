9d6b87f9b49c25e43d13f3e8251fe03aefaa2e4c
mhaii <bloo20xd@gmail.com>
2015-12-18 22:34:25 +0700

    streamline variables

--- mecha/motor.ino
+++ mecha/motor.ino
@@ -1,6 +1,6 @@
 class Motor{
   private:
-    int a,b,c, s = 0;
+    int a,b,c, s = 250;
 
   public:
     Motor(int a, int b, int c){
@@ -9,7 +9,7 @@ class Motor{
     }
     int speed(){ return s; }
     void stop(){ dw(a, LOW); dw(b, LOW);  dw(c, LOW); }
-    void reverse(int v) { dw(b, LOW); dw(a, HIGH); aw(c, v); }
-    void forward(int v) { dw(a, LOW); dw(b, HIGH); aw(c, v); }
+    void rv(int v = 0) { dw(b, LOW); dw(a, HIGH); aw(c, v?v:s); }
+    void fw(int v = 0) { dw(a, LOW); dw(b, HIGH); aw(c, v?v:s); }
 };
 
--- mecha/zebraliger.ino
+++ mecha/zebraliger.ino
@@ -5,51 +5,28 @@ int incomingByte = 0;
 int speed_val = 250;
 
 void setup() {
-  // put your setup code here, to run once:
-  BTSerial.begin(115200);
-  BTSerial.println("Robot program started");
-
-  // initialize digital pin 6-8 as output for wheel control
-  pinMode(6, INPUT);
-  pinMode(7, INPUT);
-  pinMode(8, INPUT);
+  bt.begin(115200);
+  bt.println("Robot program started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (BTSerial.available() > 0) {
-    incomingByte = BTSerial.read();
-    BTSerial.print("I received: ");
-    BTSerial.println(incomingByte);
+  if (bt.available() > 0) {
+    incomingByte = bt.read();
+    bt.println(incomingByte);
     
     // delay 10 milliseconds to allow serial update time
     delay(10);
 
     if (incomingByte == 116) {
       for (int ms = 0; ms < 2000; ms++) { // Track line for 2s
+        // Turn right
+        if (opto.readL() && opto.readR() && opto.readC()){ m1.fw(); m2.fw(m2.speed - 200); }
+        // Turn left
+        if (opto.readL() && !opto.readR() && opto.readC()){ m1.fw(m1.speed - 200); m2.fw(); }
+        // Forward
+        if (opto.readC()) { m1.fw(); m2.fw(); }
 
-        if (opto.readL && opto.readR && opto.readC) { // Turn right
-          m1.forward(speed_val);
-          //m2.reverse(speed_val);
-          m2.forward(speed_val - 200);
-        }
-
-        if (opto.readR && opto.readL && opto.readC) { // Turn left
-          //m1.reverse(speed_val);
-          m1.forward(speed_val - 200);
-          m2.forward(speed_val);
-        }
-
-        if (opto.readC) {  // Forward
-          m1.forward(speed_val);
-          m2.forward(speed_val);
-        }
-
-        while (opto.readR && opto.readL && opto.readC)) {
-          m1.reverse(speed_val);
-          m2.forward(speed_val);
-
-        }
+        while (opto.readR() && opto.readL() && opto.readC()) { m1.rv(); m2.fw(); }
         delay(10);
       }
     } else {
