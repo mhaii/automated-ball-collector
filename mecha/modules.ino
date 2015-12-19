096c4ea0de7bd8dab97ba7e528a829caca0ac936
mhaii <chess4G63>
2015-12-19 21:04:57 +0700

    combine modules into one file

--- mecha/modules.ino
+++ mecha/modules.ino
@@ -13,3 +13,16 @@ class Motor{
     void fw(int v = 0) { dw(a, LOW); dw(b, HIGH); aw(c, v?v:s); }
 };
 
+class Optosensor{
+  private:
+    int l, c, r;
+  public:
+    Optosensor(int l, int c, int r){
+      this->l = l; this->c = c; this->r = r;
+      pm(l, 0); pm(c, 0); pm(r, 0);
+    }
+    bool readL(){ return digitalRead(l); }
+    bool readC(){ return digitalRead(c); }
+    bool readR(){ return digitalRead(r); }
+};
+
